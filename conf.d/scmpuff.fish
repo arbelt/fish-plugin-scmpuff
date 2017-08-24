f73f56bf19b2e58fdf57fd3ee927f8cfc4745d89
Albert Z Wang <albert.zhao.wang@gmail.com>
2017-08-24 10:43:03 -0400

    move git function

--- conf.d/scmpuff.fish
+++ conf.d/scmpuff.fish
@@ -4,6 +4,32 @@ if type -q hub
     set -x SCMPUFF_GIT_CMD "hub"
 end
 
+
+if not type -q scmpuff
+    exit 1
+end
+
 functions -e git
 alias gs=scmpuff_status
 
+function git
+    type -q $SCMPUFF_GIT_CMD; or set -x SCMPUFF_GIT_CMD (which git)
+
+    if test (count $argv) -eq 0
+        eval $SCMPUFF_GIT_CMD
+        set -l s $status
+        return $s
+    end
+
+    switch $argv[1]
+    case commit blame log rebase merge
+        eval (scmpuff expand -- "$SCMPUFF_GIT_CMD" $argv)
+    case checkout diff rm reset
+        eval (scmpuff expand --relative -- "$SCMPUFF_GIT_CMD" $argv)
+    case add
+        eval (scmpuff expand -- "$SCMPUFF_GIT_CMD" $argv)
+        scmpuff_status
+    case '*'
+        eval "$SCMPUFF_GIT_CMD" $argv
+    end
+end
